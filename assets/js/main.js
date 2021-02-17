24e0017dd2164c6641ec30f4bcda287bfaa7dad7
Tuana <tuanacetinkaya7@gmail.com>
2021-02-17 20:32:27 +0300

    reform

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,82 +1,88 @@
-(function($) {
-  
-  "use strict";  
+(function ($) {
+  "use strict";
 
-  $(window).on('load', function() {
-
-  /*Page Loader active
+  $(window).on("load", function () {
+    /*Page Loader active
     ========================================================*/
-    $('#preloader').fadeOut();
+    $("#preloader").fadeOut();
 
-  // Sticky Nav
-    $(window).on('scroll', function() {
-        if ($(window).scrollTop() > 200) {
-            $('.scrolling-navbar').addClass('top-nav-collapse');
-        } else {
-            $('.scrolling-navbar').removeClass('top-nav-collapse');
-        }
+    // Sticky Nav
+    $(window).on("scroll", function () {
+      if ($(window).scrollTop() > 200) {
+        $(".scrolling-navbar").addClass("top-nav-collapse");
+        document.getElementById("navbar-logo").src = "assets/img/logo.png";
+      } else {
+        $(".scrolling-navbar").removeClass("top-nav-collapse");
+        document.getElementById("navbar-logo").src =
+          "assets/img/collapselogo.png";
+      }
     });
 
     /* ==========================================================================
        countdown timer
        ========================================================================== */
-     jQuery('#clock').countdown('2018/06/21',function(event){
-      var $this=jQuery(this).html(event.strftime(''
-      +'<div class="time-entry days"><span>%-D</span> Days</div> '
-      +'<div class="time-entry hours"><span>%H</span> Hours</div> '
-      +'<div class="time-entry minutes"><span>%M</span> Minutes</div> '
-      +'<div class="time-entry seconds"><span>%S</span> Seconds</div> '));
+    jQuery("#clock").countdown("2018/06/21", function (event) {
+      var $this = jQuery(this).html(
+        event.strftime(
+          "" +
+            '<div class="time-entry days"><span>%-D</span> Days</div> ' +
+            '<div class="time-entry hours"><span>%H</span> Hours</div> ' +
+            '<div class="time-entry minutes"><span>%M</span> Minutes</div> ' +
+            '<div class="time-entry seconds"><span>%S</span> Seconds</div> '
+        )
+      );
     });
 
     /* slicknav mobile menu active  */
-    $('.mobile-menu').slicknav({
-        prependTo: '.navbar-header',
-        parentTag: 'liner',
-        allowParentLinks: true,
-        duplicate: true,
-        label: '',
-      });
+    $(".mobile-menu").slicknav({
+      prependTo: ".navbar-header",
+      parentTag: "liner",
+      allowParentLinks: true,
+      duplicate: true,
+      label: "",
+    });
 
-      /* WOW Scroll Spy
+    /* WOW Scroll Spy
     ========================================================*/
-     var wow = new WOW({
+    var wow = new WOW({
       //disabled for mobile
-        mobile: false
+      mobile: false,
     });
     wow.init();
 
     /* Nivo Lightbox 
     ========================================================*/
-    $('.lightbox').nivoLightbox({
-        effect: 'fadeScale',
-        keyboardNav: true,
-      });
+    $(".lightbox").nivoLightbox({
+      effect: "fadeScale",
+      keyboardNav: true,
+    });
 
-    // one page navigation 
-    $('.navbar-nav').onePageNav({
-            currentClass: 'active'
-    }); 
+    // one page navigation
+    $(".navbar-nav").onePageNav({
+      currentClass: "active",
+    });
 
     /* Back Top Link active
     ========================================================*/
-      var offset = 200;
-      var duration = 500;
-      $(window).scroll(function() {
-        if ($(this).scrollTop() > offset) {
-          $('.back-to-top').fadeIn(400);
-        } else {
-          $('.back-to-top').fadeOut(400);
-        }
-      });
+    var offset = 200;
+    var duration = 500;
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > offset) {
+        $(".back-to-top").fadeIn(400);
+      } else {
+        $(".back-to-top").fadeOut(400);
+      }
+    });
 
-      $('.back-to-top').on('click',function(event) {
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: 0
-        }, 600);
-        return false;
-      });
-
-  });      
-
-}(jQuery));
+    $(".back-to-top").on("click", function (event) {
+      event.preventDefault();
+      $("html, body").animate(
+        {
+          scrollTop: 0,
+        },
+        600
+      );
+      return false;
+    });
+  });
+})(jQuery);
